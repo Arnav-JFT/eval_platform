191ad374aaa4aebee34bd4c4a3334e418bbde515
Arnav-JFT <arnav.sharma@jellyfishtechnologies.com>
2022-12-27 13:29:02 +0530

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
+
+  const config = new DocumentBuilder()
+    .setTitle('Eval Platform Api')
+    .setDescription('Admin user apis')
+    .setVersion('1.0')
+    .addTag('Code')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.setViewEngine('ejs');
 
   await app.listen(3000);
